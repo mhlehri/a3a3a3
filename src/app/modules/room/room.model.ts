8d05590069697c65e8d77df1f232744f4f16c36d
mhlehri <lehrimirza101@gmail.com>
2024-06-12 02:59:08 +0600

    add room interface and model

---
+++ src/app/modules/room/room.model.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+import { TRoom } from "./room.interface";
+
+const RoomSchema = new Schema<TRoom>({
+  name: { type: String, required: true },
+  roomNo: { type: String, required: true },
+  floorNo: { type: Number, required: true },
+  capacity: { type: Number, required: true },
+  pricePerSlot: { type: Number, required: true },
+  amenities: { type: [String], required: true },
+  isDeleted: { type: Boolean, default: false },
+});
+
+export const Room = model<TRoom>("Room", RoomSchema);
